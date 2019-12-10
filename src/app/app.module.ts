9556ee2f9ccb5aaeb052b2f6df6c9ecf760cfc4c
sbauer <sebauer.30.96@hotmail.com>
2019-12-10 09:52:24 -0300

    finish styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
